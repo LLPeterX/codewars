d32c41c24357efd3ab979eaf3ac831d486963372
Peter Lebedev <LPeter@mail.ru>
2022-02-09 15:45:00 +0300

    8kyu - Total amount of points

---
+++ 7kyu/totalAmountOfPoints.js
@@ -0,0 +1,42 @@
+/* 
+8kyu - Total amount of points
+https://www.codewars.com/kata/5bb904724c47249b10000131/train/javascript
+
+Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+
+if x>y - 3 points
+if x<y - 0 point
+if x=y - 1 point
+Notes:
+
+there are 10 matches in the championship
+0 <= x <= 4
+0 <= y <= 4
+*/
+
+function points(games) {
+  return games.reduce((sum, s) => {
+    let [x, y] = s.split(':');
+    if (x > y) sum += 3;
+    else if (x === y) sum++;
+    return sum;
+  }, 0);
+}
+
+console.log(points(["1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"]), 30);
+console.log(points(["1:1", "2:2", "3:3", "4:4", "2:2", "3:3", "4:4", "3:3", "4:4", "4:4"]), 10);
+console.log(points(["0:1", "0:2", "0:3", "0:4", "1:2", "1:3", "1:4", "2:3", "2:4", "3:4"]), 0); // fail
+console.log(points(["1:0", "2:0", "3:0", "4:0", "2:1", "1:3", "1:4", "2:3", "2:4", "3:4"]), 15);
+console.log(points(["1:0", "2:0", "3:0", "4:4", "2:2", "3:3", "1:4", "2:3", "2:4", "3:4"]), 12);
+// maybe that: 1:0,2:0,3:0,4:0,2:1,3:1,4:1,3:2,4:2,4:3"
+
+// best
+/* 
+const points=games=>games.reduce((output,current)=>{
+    return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
+  },0)
+*/
