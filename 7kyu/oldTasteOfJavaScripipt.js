6d35e6382717ab049798f552614855fdbad13789
Peter Lebedev <LPeter@mail.ru>
2022-12-16 15:35:54 +0300

    7kyu - An old taste of JavaScript

---
+++ 7kyu/oldTasteOfJavaScripipt.js
@@ -0,0 +1,15 @@
+/* 
+7kyu - An old taste of JavaScript
+https://www.codewars.com/kata/6359bf1600fba287b6189f9b
+
+Your code :
+
+ - must only contain characters from the following list:
+   #, >, +, !, ?, -, ~, @, <
+- must compile
+- must not be empty
+
+that is all.
+*/
+
+<--@-->
