e1b908b722749cdb829d28aa55ae58403684cee0
Peter Lebedev <LPeter@mail.ru>
2022-12-19 19:23:26 +0300

    3kyu - Battleship field validator

--- 3kyu/battleship.js
+++ 3kyu/battleship.js
@@ -1,6 +1,6 @@
 /* 
+3kyu - Battleship field validator
 https://www.codewars.com/kata/52bb6539a4cf1b12d90005b7/train/javascript
-Battleship field validator
 */
 
 function validateBattlefield(field) {
@@ -22,31 +22,31 @@ function validateBattlefield(field) {
   };
 
   let generateNeighbors = (r, c) => {
-    let offsets = [ [-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [-1, 1], [1, -1], [1, 1] ];
+    let offsets = [[-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [-1, 1], [1, -1], [1, 1]];
     return offsets.reduce((acc, offset) => {
       let nbr = r + offset[0];
       let nbc = c + offset[1];
       if (nbr >= 0 && nbr < 10 && nbc >= 0 && nbc < 10) {
-        acc.push ({r: nbr, c: nbc });
+        acc.push({ r: nbr, c: nbc });
       }
       return acc;
     }, []);
   };
 
   let hasCorrectNumShip = (components, size, expectedNumShip) => {
-      return components.filter((c) => { return c.length === size; })
-        .length === expectedNumShip;
+    return components.filter((c) => { return c.length === size; })
+      .length === expectedNumShip;
   };
 
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
-      queue.push({r: i, c: j});
+      queue.push({ r: i, c: j });
       components = [];
       while (queue.length > 0) {
-        let {r, c} = queue.shift();
+        let { r, c } = queue.shift();
         if (field[r][c] === 1 && !visited[r][c]) {
           visited[r][c] = true;
-          components.push({r, c});
+          components.push({ r, c });
           queue = queue.concat(generateNeighbors(r, c));
         }
       }
@@ -58,9 +58,9 @@ function validateBattlefield(field) {
   if (!wrongShape) {
 
     return hasCorrectNumShip(component, 4, 1) &&
-            hasCorrectNumShip(component, 3, 2) &&
-            hasCorrectNumShip(component, 2, 3) &&
-            hasCorrectNumShip(component, 1, 4);
+      hasCorrectNumShip(component, 3, 2) &&
+      hasCorrectNumShip(component, 2, 3) &&
+      hasCorrectNumShip(component, 1, 4);
   }
   return false;
-}git
+}
