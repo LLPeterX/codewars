9c95ff122e49214b46c2660c122490cdab40699d
Peter Lebedev <LPeter@mail.ru>
2022-12-21 13:01:58 +0300

    4kyu - Multiplying numbers as strings

--- 4kyu/multiplyAsString.js
+++ 4kyu/multiplyAsString.js
@@ -20,17 +20,45 @@ function multiply(a, b) {
 
   for (let i = 0; i < res.length; i++) {
     let digit = res[i] % 10;
-    let remainder = Math.floor(res[i] / 10);
+    let shift = ~~(res[i] / 10);
     res[i] = digit;
-
-    if (res[i + 1]) res[i + 1] += remainder;
-    else if (remainder != 0) res[i + 1] = remainder;
+    if (res[i + 1]) res[i + 1] += shift;
+    else if (shift != 0) res[i + 1] = shift;
   }
-  let result = res.reverse().join``;
-  while (result[0] === '0' && result.length > 1) result = result.slice(1);
-  return result;
+  return res.reverse().join("").replace(/^0+/g, '') || '0'
 }
 
 console.log(multiply("28", "341"), "9548");
 console.log(multiply("123", "456"), "56088"); // FAIL
 
+// best
+
+/* 
+function multiply(a, b) {
+  var aa = a.split('').reverse();
+  var bb = b.split('').reverse();
+
+  var stack = [];
+
+  for (var i = 0; i < aa.length; i++) {
+    for (var j = 0; j < bb.length; j++) {
+      var m = aa[i] * bb[j];
+      stack[i + j] = (stack[i + j]) ? stack[i + j] + m : m;
+    }
+  }
+
+  for (var i = 0; i < stack.length; i++) {
+    var num = stack[i] % 10;
+    var move = Math.floor(stack[i] / 10);
+    stack[i] = num;
+
+    if (stack[i + 1])
+      stack[i + 1] += move;
+    else if (move != 0)
+      stack[i + 1] = move;
+  }
+
+
+  return stack.reverse().join('').replace(/^(0(?!$))+/, "");
+}
+*/
