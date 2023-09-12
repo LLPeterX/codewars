d3d9d61b7fa044b91029ab118ce190a1f946d7de
Peter Lebedev <LPeter@mail.ru>
2023-09-12 15:46:27 +0300

    6kyu - Find heavy ball - level: conqueror

--- 6kyu/findBall.js
+++ 6kyu/findBall.js
@@ -48,3 +48,20 @@ class Balls {
 
 let scales = new Balls([2, 1, 1, 1, 1, 1, 1, 1]);
 console.log(findBall(scales, 0));
+
+// best
+/* 
+function findBall(scales) {
+  var balls = [0,1,2,3,4,5,6,7];
+  while (balls.length > 1) {
+    scales.leftPan = balls.splice(balls.length/2|0);
+    scales.rightPan = balls;
+    balls = scales.weight > 0 ? scales.rightPan : scales.leftPan;
+  }
+  return balls[0];
+}
+*/
+
+/* 
+findBall=(s,a=[0,1,2],b=[3,4,5],c=[6,7,8],d=([a,b,c],d=s.getWeight([a],[b]))=>d==-1?a:d==1?b:c,e=s.getWeight(a,b))=>d(e==-1?a:e==1?b:c)
+*/
