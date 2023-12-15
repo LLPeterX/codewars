b1093f2e3f4edec6b5ca59b990412a6f0057abab
Peter Lebedev <LPeter@mail.ru>
2023-12-15 13:22:00 +0300

    items[] done. Todo: enhance

--- 5kyu/Character.js
+++ 5kyu/Character.js
@@ -88,6 +88,7 @@ class Character {
 
     return new Proxy(this, {
       get: (target, name) => {
+        target._tmpName = name;
         if (!(name in target)) {
           target._tmpName = name;
           Reflect.defineProperty(target, name, { value: target.addItem });
@@ -98,18 +99,20 @@ class Character {
   } // end constructor
 
   addItem(str, dex, int, extra = 0) {
-    //console.log('call addItem', this._tmpName);
+    console.log('call addItem', arguments, 'name=', this._tmpName);
     //const param2str = n => "- +"[Math.sign(n) + 1] + n;
     if (this._tmpName) {
       if (Item.isWeapon(this._tmpName)) {
-        if (this.weapon.name === this._tmpName) {
-          // enhance
-          this.weapon.enhance(str, dex, int, extra);
+        let existingWeapon = this.items.find(item => item.name === this._tmpName);
+        if (existingWeapon) {
+          existingWeapon.enhance(str, dex, int, extra);
         } else {
-          let newWeapon = new Weapon(this._tmpName, str, dex, int, extra);
-          if (this.getDamage(newWeapon) > this.getDamage(this.weapon)) {
-            console.log('set new weapon ', this._tmpName);
-            this.weapon = newWeapon;
+          this.items.push(new Weapon(this._tmpName, str, dex, int, extra));
+        }
+        // find max weapon
+        for (let w of this.items) {
+          if (w instanceof (Weapon) && this.getDamage(w) > this.getDamage(this.weapon)) {
+            this.weapon = w;
           }
         }
         this.log.push(`${this.name} finds '${Item.fullName(this._tmpName)}'`);
@@ -121,12 +124,14 @@ class Character {
         if (stateValues.every(s => s === 0)) return Item.fullName(this._tmpName);
         let logStr = Item.fullName(this._tmpName) + ': ';
         let statesStr = stateValues.map((v, i) => v ? `${Character.states[i]} ${v > 0 ? '+' : ''}${v}` : '').filter(Boolean).join(' ');
+        this.items.push(new Item(this._tmpName, str, dex, int));
         this.log.push(logStr + statesStr);
       }
-      //Reflect.deleteProperty(this, this._tmpName);
+      Reflect.deleteProperty(this, this._tmpName);
       this._tmpName = null;
     }
   }
+
 
   getDamage(weapon = this.weapon) {
     return weapon.str * this.strength +
@@ -153,15 +158,17 @@ class Character {
 
 const kroker = new Character({ name: 'Kroker', strength: 15, intelligence: 7 });
 //const res = `Kroker\nstr 15\ndex 10\nint 7\nlimbs 32 dmg`;
-console.log('initial', kroker.characterInfo());
+console.log('initial:\n', kroker.characterInfo());
 //console.log(kroker.strength);
 
 kroker.axeOfFire(3, 1, 0, 20);
+console.log('\nafter axe:\n', kroker.characterInfo()); // 75 dmg - OK
+console.log(kroker.eventLog()); // "Kroker finds 'Axe of fire'"
 kroker.staffOfWater(1, 0, 2, 60);
-console.log('\nafter staff', kroker.characterInfo());
+console.log('\nafter staff:\n', kroker.characterInfo()); // 89 dmg
 kroker.axeOfFire(1, 2, 1, 10);
-console.log('\nafter 2 axe', kroker.characterInfo());
+console.log('\nafter 2 axe:\n', kroker.characterInfo()); // 92 dmg
 kroker.strangeFruit(-2, 0, 2);
-console.log('\nafter fruit', kroker.characterInfo());
-//console.log(kroker.characterInfo()); // str 15 dex 10 int 7 axe 75
+console.log('\nafter fruit:\n', kroker.characterInfo());
+console.log(kroker.characterInfo()); // 91 dmg
 console.log(kroker.eventLog()); // str 15 dex 10 int 7 axe 75
