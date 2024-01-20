49d86f6d9095f29f8d4ecc394addf7256cce7a50
Peter Lebedev <LPeter@mail.ru>
2024-01-20 18:04:35 +0300

    solved

--- 5kyu/procArrInt.js
+++ 5kyu/procArrInt.js
@@ -33,43 +33,109 @@ function isPrime(num) {
   return true;
 }
 
-console.log(isPrime(76));
-
-// const isPrime = num => {
-//   for (let i = 2; i <= Math.sqrt(num); i++) if (num % i === 0) return false;
-//   return true;
-// }
 
 function procArrInt(listNum) {
   const divisors = {};
-  let primes = 0;
+  let primesCount = 0;
   for (let n of listNum) {
-    if (isPrime(n)) {
-      console.log(`  -- prime ${n}`);
-      primes++; // попробовать сократить в низлежащем цикле
-    }
+    if (isPrime(n)) primesCount++;
     let divCount = 0;
-    //let isPrime = true;
-    for (let i = 1; i <= Math.sqrt(n); i++) {
-      if (n % i == 0) divCount++;
-      //if (!isPrime && n > 2 && n % i == 0) isPrime = false;
-    }
-    if (!divisors[divCount]) {
-      divisors[divCount] = [];
-    }
+    for (let i = 1; i <= n / 2; i++) if (n % i == 0) divCount++;
+    if (!divisors[divCount]) divisors[divCount] = [];
     divisors[divCount].push(n);
-    //primes += isPrime;
   }
-  //console.log(divisors);
-  return [listNum.length, primes]
-
+  let o = Object.entries(divisors).at(-1);
+  return [listNum.length, primesCount, [+o[0] + 1, o[1].sort((a, b) => a - b)]];
 }
+
+
+
 console.log(procArrInt([66, 36, 49, 40, 73, 12, 77, 78, 76, 8, 50,
   20, 85, 22, 24, 68, 26, 59, 92, 93, 30]), [21, 2, [9, [36]]]);
 
 
-// let list1 = [386, 5, 149, 278, 160, 91, 424, 429, 305, 436,
-//   52, 58, 188, 190, 288, 72, 458, 460, 461, 462, 207, 211, 214,
-//   335, 356, 381];
-// let result1 = [26, 4, [18, [288]]];
-// console.log(procArrInt(list1), result1);
+let list1 = [386, 5, 149, 278, 160, 91, 424, 429, 305, 436,
+  52, 58, 188, 190, 288, 72, 458, 460, 461, 462, 207, 211, 214,
+  335, 356, 381];
+let result1 = [26, 4, [18, [288]]];
+console.log(procArrInt(list1), result1);
+
+var list2 = [258, 402, 279, 153, 423, 424, 430,
+  49, 51, 58, 193, 199, 330, 369, 465, 468, 469,
+  86, 352, 97, 99, 358, 364, 113, 499, 372];
+var result2 = [26, 5, [18, [468]]];
+console.log(procArrInt(list2), result2);
+
+var list3 = [5, 396, 397, 145, 274, 286, 159,
+  422, 169, 44, 303, 433, 310, 450, 324, 326, 91,
+  226, 229, 233, 106, 237, 499, 126];
+var result3 = [24, 6, [18, [396, 450]]];
+console.log(procArrInt(list3), result3);
+
+var list4 = [641, 976, 517, 263, 136, 270, 941,
+  400, 915, 790, 539, 414, 816, 546, 675, 548, 299,
+  812, 45, 432, 392, 178, 310, 439, 956, 319, 450,
+  566, 72, 74, 715, 336, 723, 854, 472, 996, 604,
+  861, 182, 609, 117, 484, 358, 1000, 363, 237,
+  111, 629, 759, 505, 510];
+var result4 = [51, 4, [20, [336, 432, 816]]];
+console.log(procArrInt(list4), result4);
+
+// best
+
+/* 
+function isPrime(n) {
+  if (n < 2) return false;
+  if (n == 2) return true;
+  if (n % 2 === 0) return false;
+  for (let i = 3; i < n ** 0.5 + 1; i += 2) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+
+function divs(n) {
+  let r = new Set();
+  for (let i = 0; i < n ** 0.5 + 1; i++) {
+    if (n % i === 0) {
+      r.add(i);
+      r.add(n / i);
+    }
+  }
+  return r.size;
+}
+
+function procArrInt(a) {
+  let x = a.map((e) => [e, divs(e)]).sort((a, b) => b[1] - a[1]);
+  return [
+    a.length,
+    a.filter(isPrime).length,
+    [
+      x[0][1],
+      x
+        .filter((e) => e[1] === x[0][1])
+        .map((e) => e[0])
+        .sort((a, b) => a - b),
+    ],
+  ];
+}
+
+*/
+
+/* 
+const procArrInt = listNum => {
+  const countDivisors = num => {
+    for (let i = 1, count = 0, sqRoot = num ** .5; ; num % i++ || count++)
+      if (i >= sqRoot) return count * 2 + (i === sqRoot);
+  }
+  
+  const listDivs = listNum.map(countDivisors);
+  
+  const ttlNums          = listNum.length;
+  const ttlPrimes        = listDivs.filter(val => val === 2).length;
+  const highestDivCount  = Math.max(...listDivs);
+  const highestDivNums   = listNum.filter((_, idx) => listDivs[idx] === highestDivCount).sort((a, b) => a - b);
+  
+  return [ttlNums, ttlPrimes, [highestDivCount, highestDivNums]];
+}
+*/
