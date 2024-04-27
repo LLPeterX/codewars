9ffa340893d9c19ffd641cebecebfa53a19a3bff
Peter Lebedev <LPeter@mail.ru>
2024-04-27 16:56:11 +0300

    eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "no-unused-vars" : "off",
+    "no-undef" : "off"
+  }
+};
